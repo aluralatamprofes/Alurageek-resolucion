1558a5a22edd907b98a4ed6c5d57c3c761a5eba1
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-03-10 11:23:18 -0300

    corregido nombre de clases

--- controllers/productos-controller.js
+++ controllers/productos-controller.js
@@ -4,11 +4,11 @@ import { formatPrice } from "../formatterPrices.js";
 const nuevoProduto = (name, price, imageUrl, id) => {
   const card = document.createElement("div");
   const contenido = `
-        <div class="produto">
+        <div class="producto">
             <img src="${imageUrl}" alt="img">
             <h1 class="product-name"> ${name} </h1>
-            <p class="preco">${formatPrice(price)}</p>
-            <a class="ver-produto" href="../produto.html?id=${id}">Ver Produto</a>
+            <p class="precio">${formatPrice(price)}</p>
+            <a class="ver-producto" href="../produto.html?id=${id}">Ver Produto</a>
         </div>   
     `;
   card.innerHTML = contenido;
